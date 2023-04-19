57bd3724514477262feb2a9f8bfb5e4f19524132
AtharvaShinde1 <7551837-AtharvaShinde1@users.noreply.replit.com>
2023-04-19 14:32:12 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,35 @@
+class Main {
+  public static void main(String[] args) {
+    // Typecasting
+    /*
+     * // Widening Tyepcasting
+     * int a = 10;
+     * long b = a;
+     * // Explicit Typecastin
+     * long c = 10000;
+     * int d = (int)c;
+     */
+
+    // Strings in Java
+    /*
+     * String myString = "This is Java";
+     * System.out.println(myString);
+     * System.out.println(myString.charAt(2));
+     */
+
+    // Variables in Java;
+    // int num = 10;
+    // int num2 = 30;
+    // int num3 = 90;
+
+    // DataTypes -
+    // Primitive - int, float, char, etc
+    // Non-Primitive - string, arrays, user defined
+
+    // char c = '6';
+    // System.out.println(c);
+
+    // long l = 30000000000000L;
+    // System.out.println(l);
+  }
+}
