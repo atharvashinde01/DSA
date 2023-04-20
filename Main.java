ebe6008226de466f5b1a24ac447a0fde77ffbf2f
atharvashinde01 <66786964+atharvashinde01@users.noreply.github.com>
2023-04-20 06:29:19 +0000

    Updated

--- Main.java
+++ Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
+    // Simple Interest Calculator
+
+    Scanner sc = new Scanner(System.in);
+    int p = sc.nextInt();
+    int r = sc.nextInt();
+    int t = sc.nextInt();
+
+    int si = (p*r*t)/100;
+    System.out.println("Simple Interest = " + si);
+    
+    // Inputs - Scanner
+    /*
+    Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt();
+    System.out.println(x*x);
+    */
+
+    // Constant in Java
+    /*
+    final int a = 1;
+    System.out.println(a);
+
+    a = 6;
+    System.out.println(a);
+    */
+    
     // Typecasting
     /*
      * // Widening Tyepcasting
